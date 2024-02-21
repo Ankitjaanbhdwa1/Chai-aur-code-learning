3dca7cce71799a067a53eaec8c518a7e30dd1546
Ankitjaanbhdwa1 <160657921+Ankitjaanbhdwa1@users.noreply.github.com>
2024-02-21 15:17:44 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 12445
+var accountEmail = "amarjeetchoudhary647@gmail.com"
+let accountPassword = "13245"
+accountCity = "delhi"
+let accountState
+
+// accountId = 2 // not allowed
+
+accountEmail = "amarjeet2@gmail.com"
+accountPassword = "2121221"
+accountCity = "jaipur"
+
+console.log(accountId);
+
+/*prefer not to use var
+because of isuue in block scope and function scope
+*/
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])

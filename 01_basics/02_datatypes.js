da518db8443fd8a2e42baf1f752637b520560f5f
Ankitjaanbhdwa1 <160657921+Ankitjaanbhdwa1@users.noreply.github.com>
2024-02-22 18:35:49 +0000

    container config

--- 01_basics/02_datatypes.js
+++ 01_basics/02_datatypes.js
@@ -1,3 +1,28 @@
 "use strict"; // treat all js as a new java version
 
 //alert(3 + 3)//we are using nodejs, not browser
+
+console.log(3 
+    + 
+    3); // code readability should be high//
+
+console.log("Amarjeet");
+
+let name = "Amarjeet"
+let age = 18
+let isloggedIn = false
+
+//*premitive data types*//
+
+//number => 2 to power 53, its a range
+//bigint => it uses with big range like facebook reddit
+//string => ""
+//boolean => true/false
+//null => its a standalone value, it a type(empty)
+//undefined => value doesn't defined, variable is declared but and value does't assign
+//symbol =>  for uses uniqueness,used by figma tools for defining which is individual component, mostly uses for uniquness
+
+//object(null)
+
+console.log(typeof undefined);//undefined, because it is a type
+ console.log(typeof null);//object, because its type is object 

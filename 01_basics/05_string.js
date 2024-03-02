6d5f33e945e9e88b5a1f76c21fb9efdbebd1a200
Ankitjaanbhdwa1 <160657921+Ankitjaanbhdwa1@users.noreply.github.com>
2024-03-02 14:38:54 +0000

    container config

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -1,4 +1,8 @@
 const name = "amarjeet"
-const repoCount = "50"
+const repoCount = 50
 
-console.log(name + repoCount + "value");
+//console.log(name + repoCount + "value"); outdated dont have to use 
+
+console.log('Hello my name is ${name} and my repo count is ${repocount}');
+
+const gameName = new String('amarjeet')

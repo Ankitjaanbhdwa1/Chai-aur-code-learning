c070c3e600d3cfa6330996a579c2e48c237dd404
Ankitjaanbhdwa1 <160657921+Ankitjaanbhdwa1@users.noreply.github.com>
2024-03-02 15:46:04 +0000

    container config

--- 01_basics/05_string.js
+++ 01_basics/05_string.js
@@ -5,4 +5,27 @@ const repoCount = 50
 
 console.log('Hello my name is ${name} and my repo count is ${repocount}');
 
-const gameName = new String('amarjeet')
+const gameName = new String('amar-je-et')
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(5));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherstring = gameName.slice(-5, 4)
+// console.log(anotherstring);
+
+const newStringOne = "  amarjeet  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https//:amarjeetchoudhary%20choudharydotcom"
+console.log(url.replace('%20', '-'));
+
+console.log(url.includes('sundar'));
+console.log(gameName.split('-'));
